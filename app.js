f0e246c2f9546fbabd8010e52d72fbaf7c59625a
Samuel Gutierrez <69909329+S535530@users.noreply.github.com>
2022-11-01 20:17:22 -0500

    created selector.js and .pug and made it run.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jamRouter = require('./routes/jam');
 var gridRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jam', jamRouter);
 app.use('/gridbuild', gridRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
